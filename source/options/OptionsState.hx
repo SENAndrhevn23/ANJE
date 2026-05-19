d14b46a2d941985d3b1c56c9313ba4a1000c9951
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-05-18 21:25:12 -0500

    Refactor OptionsState by removing unused variables

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -7,8 +7,6 @@ import backend.StageData;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.FlxCamera;
-import flixel.FlxObject;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.math.FlxMath;
 
@@ -30,19 +28,12 @@ class OptionsState extends MusicBeatState
 	];
 
 	private var grpOptions:FlxTypedGroup<Alphabet>;
-
 	private static var curSelected:Int = 0;
-
 	public static var menuBG:FlxSprite;
 	public static var onPlayState:Bool = false;
 
-	var camFollow:FlxObject;
-	var camFollowPos:FlxObject;
-
-	var mainCam:FlxCamera;
-	var funnyCam:FlxCamera;
-
-	var exiting:Bool = false;
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
 
 	function openSelectedSubstate(label:String)
 	{
@@ -85,27 +76,10 @@ class OptionsState extends MusicBeatState
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
-		mainCam = initPsychCamera();
-
-		funnyCam = new FlxCamera();
-		funnyCam.bgColor.alpha = 0;
-		FlxG.cameras.add(funnyCam, false);
-
-		camFollow = new FlxObject(0, 0, 1, 1);
-		camFollowPos = new FlxObject(0, 0, 1, 1);
-
-		add(camFollow);
-		add(camFollowPos);
-
-		funnyCam.follow(camFollowPos);
-
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		bg.color = 0xFFea71fd;
-
-		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
-
 		bg.screenCenter();
 		add(bg);
 
@@ -114,23 +88,19 @@ class OptionsState extends MusicBeatState
 
 		for (num => option in options)
 		{
-			var optionText:Alphabet = new Alphabet(
-				0,
-				0,
-				Language.getPhrase('options_$option', option),
-				true
-			);
-
+			var optionText:Alphabet = new Alphabet(0, 0, Language.getPhrase('options_$option', option), true);
 			optionText.screenCenter();
-			optionText.y += (100 * (num - (options.length / 2))) + 50;
-
-			optionText.cameras = [funnyCam];
-
+			optionText.y += (92 * (num - (options.length / 2))) + 45;
 			grpOptions.add(optionText);
 		}
 
-		changeSelection();
+		selectorLeft = new Alphabet(0, 0, '>', true);
+		add(selectorLeft);
 
+		selectorRight = new Alphabet(0, 0, '<', true);
+		add(selectorRight);
+
+		changeSelection();
 		ClientPrefs.saveSettings();
 
 		super.create();
@@ -139,61 +109,35 @@ class OptionsState extends MusicBeatState
 	override function closeSubState()
 	{
 		super.closeSubState();
-
 		ClientPrefs.saveSettings();
 
 		#if DISCORD_ALLOWED
 		DiscordClient.changePresence("Options Menu", null);
 		#end
-
-		persistentUpdate = true;
-		funnyCam.visible = true;
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if(exiting)
-			return;
-
 		if (controls.UI_UP_P)
-		{
-			FlxG.sound.play(Paths.sound('scrollMenu'));
 			changeSelection(-1);
-		}
 
 		if (controls.UI_DOWN_P)
-		{
-			FlxG.sound.play(Paths.sound('scrollMenu'));
 			changeSelection(1);
-		}
-
-		var lerpVal:Float = Math.max(0, Math.min(1, elapsed * 7.5));
-
-		camFollowPos.setPosition(
-			camFollowPos.x,
-			FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal)
-		);
 
 		if (controls.BACK)
 		{
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 
-			exiting = true;
-
-			if(onPlayState)
+			if (onPlayState)
 			{
 				StageData.loadDirectory(PlayState.SONG);
-
 				LoadingState.loadAndSwitchState(new PlayState());
-
 				FlxG.sound.music.volume = 0;
 			}
 			else
-			{
 				MusicBeatState.switchState(new MainMenuState());
-			}
 		}
 		else if (controls.ACCEPT)
 		{
@@ -207,27 +151,23 @@ class OptionsState extends MusicBeatState
 
 		for (num => item in grpOptions.members)
 		{
-			if(item == null)
+			if (item == null)
 				continue;
 
 			item.targetY = num - curSelected;
-
 			item.alpha = 0.6;
 
-			if (num == curSelected)
+			if (item.targetY == 0)
 			{
 				item.alpha = 1;
-
-				var thing:Float = grpOptions.members.length > 6
-					? grpOptions.members.length * 2
-					: 0;
-
-				camFollow.setPosition(
-					FlxG.width / 2,
-					item.y + 100 - thing
-				);
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
 			}
 		}
+
+		FlxG.sound.play(Paths.sound('scrollMenu'));
 	}
 
 	override function destroy()
